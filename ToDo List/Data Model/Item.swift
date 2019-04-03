06d28f45da6820798cd7102152bc47fa0f2eb42a
Mariia Vovk <mvovk@icstars.org>
2019-04-02 19:25:31 -0500

    Convert to MVC design pattern

---
+++ ToDo List/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDo List
+//
+//  Created by student2 on 4/2/19.
+//  Copyright © 2019 Masha. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
